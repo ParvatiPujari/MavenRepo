c6469c597518b0628e946131199c495367ce1b08
DELL <pujariparvati1022@gmail.com>
2025-10-07 01:24:23 +0530

    Fixed review comments

--- Project/src/test/java/testpackage/VerifyHomePage.java
+++ Project/src/test/java/testpackage/VerifyHomePage.java
@@ -1,5 +1,7 @@
 package testpackage;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
@@ -9,11 +11,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class VerifyHomePage {
 	
 	@Test
-	public void VerifyHP() throws InterruptedException {
+	public void VerifyHP() {
 		WebDriver driver = WebDriverManager.chromedriver().avoidShutdownHook().create();
 		driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
 		driver.manage().window().maximize();
-		Thread.sleep(1000);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		driver.findElement(By.cssSelector(".home")).isDisplayed();
 		
 	}
